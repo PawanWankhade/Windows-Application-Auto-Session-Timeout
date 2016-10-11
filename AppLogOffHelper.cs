513d58ec6de994b618b64b4d299d95b2875cd5a0
Pawan Wankhade <pawanwankhade@hotmail.com>
2016-10-11 14:25:26 +0530

    Prototype for session timeout
    Code added without any authentication privilege.

---
+++ AppLogOffHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinApp_SessionTimeOut
+{
+    class AutoLogOffHelper
+    {
+        public static int LogOffTime { get; set; }
+        public static int Wait { get; set; }
+        public static Timer timer = null;
+        public AutoLogOffHelper()
+        {
+
+        }
+        public delegate void MakeAutoLogOff();
+        public static event MakeAutoLogOff MakeAutoLogOffEvent;
+        public static void StartAutoLogOffOption()
+        {
+            Application.Idle += Application_Idle;
+        }
+
+        private static void Application_Idle(object sender, EventArgs e)
+        {
+            if (timer == null)
+            {
+                timer = new Timer();
+                timer.Interval = LogOffTime * 60 * 1000;
+                timer.Tick += Timer_Tick;
+                timer.Enabled = true;
+            }
+            else
+            {
+                if (timer.Enabled == false)
+                {   
+                    timer.Enabled = true;
+                }
+            }
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                timer = null;
+                if (MakeAutoLogOffEvent != null)
+                {
+                    MakeAutoLogOffEvent();
+                }
+            }
+        }
+        public static void ResetLogOffSettings()
+        {
+            if (timer != null)
+            {
+                timer.Enabled = false;
+                timer.Enabled = true;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinApp_SessionTimeOut
+{
+    public partial class Form1 : Form, IMessageFilter
+    {
+        public Form1()
+        {
+            SetSessionConfigSettings();
+            InitializeComponent();
+        }
+
+        private void SetSessionConfigSettings()
+        {
+            AutoLogOffHelper.LogOffTime = 1;
+            AutoLogOffHelper.MakeAutoLogOff eventHandler = new AutoLogOffHelper.MakeAutoLogOff(AutoLogOff_MakeAutoLogOff);
+            AutoLogOffHelper.MakeAutoLogOffEvent += eventHandler;
+            AutoLogOffHelper.StartAutoLogOffOption();
+            Application.AddMessageFilter(this);
+        }
+
+        private void AutoLogOff_MakeAutoLogOff()
+        {
+            label1.Text = "Session closed.";
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if ((m.Msg >= 0x0200 && m.Msg <= 0x020A) || (m.Msg <= 0x0106 && m.Msg >= 0x00A0) || (m.Msg == 0x00211))
+            {
+                AutoLogOffHelper.ResetLogOffSettings();
+                string time = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss fff tt");
+                label2.Text = "Main " + time;
+                label3.Text = "Timer reset on the user activity at " + time;
+                label4.Text = "Last user activity type OS message 0x" + m.Msg.ToString("X");
+            }
+                return false;
+        }
+    }
+}
